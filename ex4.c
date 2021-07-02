1430d7875db575ae77cb701158ebca8fc4c958ae
Yuki-s1270015 <86813200+Yuki-s1270015@users.noreply.github.com>
2021-07-02 12:19:49 +0900

    Initial revision

--- ex4.c
+++ ex4.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(){
   int i,num,total=0;
   
+  srand((unsigned int)time(NULL));
   printf("Rolling the dice...\n");
   
   for(i=0;i<2;i++){
-    num=rand();
+    num=1+rand()%6;
     printf("Die %d: %d\n",i+1,num);
     total=total+num;
   }
